c8f41213a35c681dda03f8bda4cf691550fe09ba
feritte <ferituzer@gmail.com>
2017-10-20 09:51:18 +0200

    Update tools.h

--- src/tools.h
+++ src/tools.h
@@ -2,7 +2,7 @@
 #define TOOLS_H_
 #include <vector>
 #include "Eigen/Dense"
-
+#include "measurement_package.h"
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 using namespace std;
@@ -23,6 +23,7 @@ public:
   * A helper method to calculate RMSE.
   */
   VectorXd CalculateRMSE(const vector<VectorXd> &estimations, const vector<VectorXd> &ground_truth);
+  float CalculateNISPerformance(const std::vector<double> &nis_values, MeasurementPackage::SensorType sensorType);
 
 };
 
